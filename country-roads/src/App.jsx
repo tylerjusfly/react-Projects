d29fa774ec89688078ed918166419d7bf5f3b433
tylerjusfly <tylerjusfly1@gmail.com>
2022-07-15 05:36:38 +0100

    country music with props

--- country-roads/src/App.jsx
+++ country-roads/src/App.jsx
@@ -8,12 +8,9 @@ export const App = () => {
   const cardElement = artistData.map((data) => {
     return (
       <Card
-        image={data.image_url}
-        alt={data.artist_names}
-        name={data.artist_names}
-        songTitle={data.title}
-        date={data.release_date}
-        verified={data.verified}
+        // making a key that is unique to each data in cards
+        key={data.lyrics}
+        props = {data}
       />
     );
   });
@@ -22,46 +19,8 @@ export const App = () => {
     <>
       <NavBar />
       <HeroTab />
-      {cardElement}
+      <div className="card---props">{cardElement}</div>
     </>
   );
 };
 
-// import { Joke } from "./components/joke/Joke"
-
-// export const App = () => {
-//     return(
-//         <>
-//         <Joke
-//         joke = 'dicta saepe voluptas maiores eveniet amet quaerat fugit vitae, odio totam sit.'
-//         isPun = {true}
-//         downvotes = {4}
-//         comments ={['nice', 'what!!!']}
-//         />
-//         <Joke
-//         setup = ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil quis rem ratione'
-//         joke = 'dicta saepe voluptas maiores eveniet amet quaerat fugit vitae, odio totam sit.'
-//         isPun = {true}
-//         downvotes = {5}
-//         />
-//         <Joke
-//         setup = ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil facere porro'
-//         joke = 'dicta saepe voluptas maiores eveniet amet quaerat fugit vitae, odio totam sit.'
-//         isPun = {false}
-//         downvotes = {10}
-//         />
-//         <Joke
-//         setup = ' Lorem, ipsum dolor sit amet elit. Nihil quis rem ratione fconsectetur adipisicing '
-//         joke = 'dicta saepe voluptas maiores eveniet amet quaerat fugit vitae, odio totam sit.'
-//         isPun = {true}
-//         downvotes = {0}
-//         />
-//         <Joke
-//         setup = ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil facere porro'
-//         joke = 'dicta saepe voluptas maiores eveniet amet quaerat fugit vitae, odio totam sit.'
-//         isPun = {false}
-//         downvotes = {10}
-//         />
-//         </>
-//     )
-// }
--- country-roads/src/components/card/Card.jsx
+++ country-roads/src/components/card/Card.jsx
@@ -1,19 +1,24 @@
 import "./card.css";
 import LukeCombs from "../../statics/images/Lukecombs.png";
 import guitarFav from '../../favicon.svg'
+import Verifiedsvg from '../../statics/images/verified.svg'
 
-export const Card = ({name, image, songTitle, date, verified, alt}) => {
+export const Card = ({props}) => {
+
+  let badgeText 
+  if(props.verified){ badgeText = 'Verified'}
+  else if(!props.verified) { badgeText = 'Not Verified'}
 
   return (
     <div className="card">
-      <img src={image} alt={alt} className="card--image" />
+      <img src={props.image_url} alt={props.artist_names} className="card--image" />
       <div className="card--stats">
-        <p className="artist--name">{name}</p>
-        <img src={guitarFav} alt="" className="card--guitar" />
-        <span className="is_verified"> "is_verified": {verified}</span>
+        <p className="artist--name">{props.artist_names}</p>
+        <span>{badgeText} : </span><img src={props.verified ? Verifiedsvg : guitarFav} alt="guitar" className= "card--guitar" />
       </div>
-        <p className="full_title"> <span className="bolderized">song_title : </span>{songTitle}</p>
-        <p className="release--date"><span className="bolderized">release_date : </span>{date}</p>
+        <p className="full_title"> <span className="bolderized">song_title : </span>{props.title}</p>
+        <p className="release--date"><span className="bolderized">release_date : </span>{props.release_date}</p>
+        <a href={props.lyrics}>View Lyrics</a>
     </div>
   );
 };
