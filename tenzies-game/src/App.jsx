99e83c7ff333fceaedde5221eadef437accc1a67
tylerjusfly <tylerjusfly1@gmail.com>
2022-08-03 20:40:02 +0100

    complete tenzies game

--- tenzies-game/src/App.jsx
+++ tenzies-game/src/App.jsx
@@ -2,19 +2,41 @@ import './assets/App.css'
 import React from 'react'
 import Die from './components/Die'
 import {nanoid} from 'nanoid'
+import Confetti from 'react-confetti'
 
 export default function App() {
+  
   function allNewDice(){
     let randomnums = []
 
     for (let i = 0; i < 10; i++) {
-      let x = Math.floor((Math.random() * 6) + 1);
-      randomnums.push({value : x, isheld : false, id : nanoid(5)})
+      randomnums.push(generateNewDie())
     }
     return randomnums
   }
+
+  function generateNewDie(){
+    return {
+      value : Math.floor((Math.random() * 6) + 1),
+      isheld : false, 
+      id : nanoid(5)
+    }
+  }
   // creating state to hold array of nums
   const [DiceNums, setDiceNums] = React.useState(allNewDice())
+
+  const [tenzies, setTenzies] = React.useState(false)
+  React.useEffect(() => {
+    let allHeld = DiceNums.every(dice => dice.isheld === true)
+    const first = DiceNums[0].value
+    let allValue = DiceNums.every(dice => dice.value === first)
+     
+    if(allHeld && allValue === true ){
+      setTenzies(true)
+    }
+    else{ setTenzies(false)}
+    
+  }, DiceNums)
 
   const diceElements = DiceNums.map(dice => {
     return (
@@ -28,7 +50,15 @@ export default function App() {
   })
 
   function reRollDice(){
-    setDiceNums(allNewDice())
+    if(!tenzies){
+      setDiceNums(oldDice => oldDice.map(die => {
+        return die.isheld == false ? generateNewDie() : die
+          }))
+    }
+    else{
+      setTenzies(false)
+      setDiceNums(allNewDice())
+    }
   }
 
   function hold(id){
@@ -39,10 +69,16 @@ export default function App() {
 
   return (
     <main>
+      {tenzies && <Confetti/>}
+      <div className="title"> Tenzies By TylerJusFly </div>
+      <div className="game--plan">
+        Roll until the dice are the same <p> Click on each die to freeze it at its current value between rolls </p>
+      </div>
+      
       <div className="die-container">
        {diceElements}
       </div>
-      <button className='roll-dice-button' onClick={reRollDice}>Roll Dice</button>
+      <button className='roll-dice-button' onClick={reRollDice}>{tenzies ? "New Game": "Roll Dice"}</button>
     </main>
   )
 }
